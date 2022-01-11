05b33322d39e3796773cc828aa16c03305a3a7d3
Shoaib Muhammad <81280268+Shooaiib-Muhammad@users.noreply.github.com>
2022-01-11 08:33:39 +0500

    com port chnaged

--- MatelDedt/MatalDed/Forms/Form1.Designer.vb
+++ MatelDedt/MatalDed/Forms/Form1.Designer.vb
@@ -84,7 +84,7 @@ Partial Class Form1
         '
         'SerialPort1
         '
-        Me.SerialPort1.PortName = "COM6"
+        Me.SerialPort1.PortName = "COM3"
         '
         'Timer1
         '
